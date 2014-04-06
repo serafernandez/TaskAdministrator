6425bacc1d3e685b60a39cf9e6a4615b04c3a293
sera1308 <serafe22loco@gmail.com>
2014-04-06 16:38:56 -0300

    Saco backbone y underscore como dependencias de node y los pongo como librerias en la carpeta js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,52 @@
-var _ = require('underscore')._,
+/*var _ = require('underscore')._,
 	Backbone = require('backbone')
+*/
+
+// Vistas
+window.ViewAgregarTarea = Backbone.View.extend({
+	events:{
+		"click .btn":"ponerTarea"
+	},
+	initialize:function(config){
+		this.$el=config.$el;
+	},
+	render:function(){
+
+	},
+	ponerTarea:function(e){
+		console.log("LAlalela")
+	}
+})
+window.ViewListas = Backbone.View.extend({
+	events:{
+		"sort li.tarea":"guardarTarea"
+	},
+	initialize:function(config){
+
+	},
+	render: function(){
+
+	},
+	guardarTarea: function(e){
+		console.log('lalal')
+	}
+})
+
+
+// Colecciones
+window.CollTareas = Backbone.Collection.extend({
+
+})
+
 
 $(document).ready(function(){
-	window.templates= {};
+	window.templates={};
+
 	$( "#ulPorHacer, #ulHaciendo, #ulHecho" ).sortable({
-      connectWith: ".listasConectadas"
+    	connectWith: ".listasConectadas"
     }).disableSelection();
+
+    window.viewAgregarTarea = new window.ViewAgregarTarea({
+    	$el:$('#ponerTarea')
+    })
 })
