17c63b92ee7ba9e57b3b5386b3164f599abbc420
sera1308 <serafe22loco@gmail.com>
2014-04-09 05:48:33 -0300

    Quedo funcionando la muestra y la guardada en la BD, falta la actualizacion cuando se ordenan

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-/*var _ = require('underscore')._,
-	Backbone = require('backbone')
-*/
+window.datastore = require('nedb')
+window.db = {}
+
+
 
 // Vistas
 window.ViewAgregarTarea = Backbone.View.extend({
 	events:{
-		"click .btn":"ponerTarea"
+		"click .btn":"ponerTarea",
+		"keypress input":"controlTeclaPresionada"
 	},
 	initialize:function(config){
 		this.$el=config.$el;
@@ -14,39 +16,100 @@ window.ViewAgregarTarea = Backbone.View.extend({
 
 	},
 	ponerTarea:function(e){
-		console.log("LAlalela")
+		if($('#inputAgregar').val()!=""){
+			//$('#inputAgregar').val("")
+			idPrevio=Math.floor((Math.random()*1000000)+1)
+			window.colPorHacer.guardar($('#inputAgregar').val(), 5, idPrevio)
+			window.colPorHacer.add({titulo:$('#inputAgregar').val(), nivel:5, _id:idPrevio})
+		}
+	},
+	controlTeclaPresionada:function(e){
+		if(e.which===13)
+			this.ponerTarea()
 	}
-})
+});
 window.ViewListas = Backbone.View.extend({
 	events:{
 		"sort li.tarea":"guardarTarea"
 	},
 	initialize:function(config){
-
+		this.$el=config.$el
+		var self=this
+		window.colPorHacer.on('add', function(model){
+			$(self.$el).find('#ulPorHacer').append(window.templates.tempTareas(model.toJSON()))
+		})
+		window.colHaciendo.on('add', function(model){
+			$(self.$el).find('#ulHaciendo').append(window.templates.tempTareas(model.toJSON()))
+		})
+		window.colHechas.on('add', function(model){
+			$(self.$el).find('#ulHechas').append(window.templates.tempTareas(model.toJSON()))
+		})
 	},
 	render: function(){
 
 	},
 	guardarTarea: function(e){
-		console.log('lalal')
+		debugger;
+		console.log(e)
 	}
-})
+});
 
 
 // Colecciones
-window.CollTareas = Backbone.Collection.extend({
-
+window.CollTareasPorHacer = Backbone.Collection.extend({
+	guardar:function(titulo, nivel, idPrevio){
+		debugger;
+		window.db.tareas.insert({titulo:titulo, nivel:nivel, columna:"porHacer"}, function(err, doc){
+			$('#'+idPrevio).attr('id', doc._id);
+		})
+	},
+	actualizarColumna:function(id){
+		window.db.tareas.update({_id:id}, {$set:{columna:'porHacer'}}, {}, function(err, num){})
+	}
 })
-
+window.CollTareasHaciendo = Backbone.Collection.extend({
+	actualizarColumna:function(id){
+		window.db.tareas.update({_id:id}, {$set:{columna:'haciendo'}}, {}, function(err, num){})
+	}
+})
+window.CollTareasHechas = Backbone.Collection.extend({
+	actualizarColumna:function(id){
+		window.db.tareas.update({_id:id}, {$set:{columna:'hechas'}}, {}, function(err, num){})
+	}
+})
 
 $(document).ready(function(){
 	window.templates={};
+	window.templates.tempTareas = _.template($('#templateTarea').html());
 
 	$( "#ulPorHacer, #ulHaciendo, #ulHecho" ).sortable({
     	connectWith: ".listasConectadas"
     }).disableSelection();
 
+    window.colPorHacer = new window.CollTareasPorHacer({})
+    window.colHaciendo = new window.CollTareasHaciendo({})
+    window.colHechas = new window.CollTareasHechas({})
+
     window.viewAgregarTarea = new window.ViewAgregarTarea({
-    	$el:$('#ponerTarea')
+    	$el:$('#agregarTarea'),
+    });
+    window.viewListas = new window.ViewListas({
+    	$el:$('#listas')
     })
+
+    window.db.tareas = new window.datastore({filename:"db/tareas.db"})
+	window.db.tareas.loadDatabase();
+
+	window.db.tareas.find({columna:"porHacer"}, function(err, docs){
+		for(i=0; i<docs.length; i++)
+			window.colPorHacer.add(docs[i])
+	})
+	window.db.tareas.find({columna:"haciendo"}, function(err, docs){
+		for(i=0; i<docs.length; i++)
+			window.colHaciendo.add(docs[i])
+	})
+	window.db.tareas.find({columna:"hechos"}, function(err, docs){
+		for(i=0; i<docs.length; i++)
+			window.colHechos.add(docs[i])
+	})
 })
